cf5b917eb16df43b64070c215e4994f1cc625ae5
Vishal Thakur <astrovishalthakur@gmail.com>
2022-02-06 21:19:04 +0530

    increase sleep time of app

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if "load_state" not in st.session_state:
 
 st.sidebar.title("Whatsapp Chat Analyzer")
 uploaded_file = st.sidebar.file_uploader("Choose a file")
-time.sleep(1)
+time.sleep(5)
 if uploaded_file is not None or st.session_state.load_state:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
